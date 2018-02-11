93a43dfc8c6ae3dbc358f913d39cc690fb99e0cd
jvales95 <36300386+jvales95@users.noreply.github.com>
2018-02-11 17:27:45 -0500

    Add files via upload

---
+++ Taller[09-02-2018]/15/15.py
@@ -0,0 +1,34 @@
+//Metodo Punto Fijo
+
+from math import *
+def g(x):
+
+    y=0.5*sqrt(10-x**3)
+
+    return y
+
+p0=0
+
+tol=0.001
+
+n0=50
+
+i=1
+
+while i<=n0:
+
+    p=g(p0)
+
+    if abs(p-p0)<tol:
+
+        print("El punto fijo es ",p," despues de ",i," iteraciones")
+
+        break
+
+    i=i+1
+
+    p0=p
+
+if i>n0:
+
+    print("El metodo no converge ")
